2ccec7e0aa702382e77ad025bd9b74bac99a4480
basudevkumar <jhulonkumar40@gmail.com>
2026-04-04 22:41:22 +0600

    feat(navbar): make responsive with custom toggle
    - Add custom hamburger button with open/close animation
    - Control collapse via useState + expanded prop
    - Hide toggler on lg breakpoint using d-lg-none
    - Close menu on nav link and button click
    - Support from 320px

--- src/component/menu/Menu.jsx
+++ src/component/menu/Menu.jsx
@@ -1,43 +1,47 @@
-import { Container, Image, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Image, Nav, Navbar } from "react-bootstrap";
 import { IoChevronDownSharp } from "react-icons/io5";
 import "./menu.css";
 import Button from "../button/Button";
 import NavImages from "../../assets/navLogo.png";
- 
+
 const Menu = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar expand="lg" className="navbarMain">
+    <Navbar expand="lg" className="navbarMain" expanded={expanded}>
       <Container>
-        <Navbar.Brand>
-          
+        <Navbar.Brand href="/">
           <Image src={NavImages} className="commonImage" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        {/* Custom Hamburger Toggle */}
+        <button
+          className={`custom-toggler ${expanded ? "open" : ""}`}
+          onClick={() => setExpanded((prev) => !prev)}
+          aria-label="Toggle navigation"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto coustomNav">
-            <Nav.Link className="openSence boldPragraph">
-              Home <IoChevronDownSharp />
-            </Nav.Link>
-            <Nav.Link className="openSence boldPragraph">
-              About <IoChevronDownSharp />
-            </Nav.Link>
-            <Nav.Link className="openSence boldPragraph">
-              Service <IoChevronDownSharp />
-            </Nav.Link>
-            <Nav.Link className="openSence boldPragraph">
-              Portfolio
-              <IoChevronDownSharp />
-            </Nav.Link>
-            <Nav.Link className="openSence boldPragraph">
-              Price <IoChevronDownSharp />
-            </Nav.Link>
-            <Nav.Link className="openSence boldPragraph">
-              Blog <IoChevronDownSharp />
-            </Nav.Link>
+            {["Home", "About", "Service", "Portfolio", "Price", "Blog"].map((item) => (
+              <Nav.Link
+                key={item}
+                className="openSence boldPragraph"
+                onClick={() => setExpanded(false)}
+              >
+                {item} <IoChevronDownSharp />
+              </Nav.Link>
+            ))}
           </Nav>
-          <Button
-            children={"Contact Us"}
-          />
+
+          <div className="contact-btn-wrapper">
+            <Button onClick={() => setExpanded(false)}>Contact Us</Button>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
